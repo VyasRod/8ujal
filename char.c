ad6d2beedaf710af2c227cfc6122d138bdfef6c7
8ujal <sujalpaikar01@gmail.com>
2025-08-13 11:44:56 +0530

    Add files via upload

---
+++ char.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+
+    char bit[50],stuff[50];
+    int count = 0, i=0, j=0;
+
+    printf("enter the bit sequence:");
+    scanf("%s",bit);
+
+    for(i=0;i<strlen(bit);i++){
+        if(bit[i] == '0')
+            count=0;
+        else if(bit[i]=='1'){
+            count++;
+            stuff[j++]=bit[i];
+        }
+        if(count==5){
+            stuff[j++]='0';
+            count=0;
+
+        }
+    }
+    stuff[j]='\0';
+
+    printf("Bit array:");
+    printf("%s\r\n,stuff");
+
+    printf("stuffed array:");
+    printf("%s\r\n,stuff");
+
+    return 0;
+
+}
+            
+        
